294300d8315bc5f70aa10049437a7f2c96f9c457
Manuel Galdon <mgaldon49@gmail.com>
2020-09-14 16:49:10 +0200

    edit for single next node

--- challenge/src/main/java/com/traverse/diagram/challenge/ModelParser.java
+++ challenge/src/main/java/com/traverse/diagram/challenge/ModelParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.camunda.bpm.engine.task.Task;
@@ -21,53 +22,121 @@ import com.sun.glass.ui.CommonDialogs.Type;
 
 public class ModelParser {
 	
+	boolean finish = false ;
+	public List<FlowNode> list;
 	public final Collection <FlowNode> path = null ; 
+	boolean hasMore = false;
+	public ArrayList<String> saved_nodes = new ArrayList<String>();
+	public ArrayList<String> start_end = new ArrayList<String>();
+	
 	public BpmnModelInstance parse() throws IOException {
 		
 		String xml = GetXml.getXml();
 		InputStream targetStream = new ByteArrayInputStream(xml.getBytes());
 
 		BpmnModelInstance modelInstance = Bpmn.readModelFromStream(targetStream);
-		final FlowNode start_node = (FlowNode) modelInstance.getModelElementById("invoice_approved");
-		final FlowNode end_node = (FlowNode) modelInstance.getModelElementById("invoiceProcessed");
+		
+		final FlowNode start_node = (FlowNode) modelInstance.getModelElementById("StartEvent_1");
+		final FlowNode end_node = (FlowNode) modelInstance.getModelElementById("prepareBankTransfer");
+		
+		start_end.add(start_node.getId());
+		start_end.add(end_node.getId());
 
-		System.out.println("Start node: " + start_node.getId());
+		System.out.println("Start node: " + start_node.getId() + "\n");
+		
+		
+
 		find(start_node, end_node);
 		return modelInstance;
+		
+	}
+	
+	public void checkIfEnd(FlowNode end, List<FlowNode> list) throws IOException {
+		
+
+		if(list.contains(end)) {
+			finish = true;
+			System.out.println("End node reached: " + end.getId() + "." + "\n\n----End of path.----\n");
+			
+			System.out.println("The path from " +start_end.get(0) + " to " + start_end.get(1) + " is \n");
+			
+			for(int i = 0; i<saved_nodes.size(); i++) {
+
+				System.out.println(saved_nodes.get(i));
+
+			}
+			
+			System.exit(-1);
+
+		}else {
+			
+			find(list.get(0), end);
+		}
 		
 	}
 	
 	public void find(FlowNode start, FlowNode end) throws IOException {
 		
 		try {
-
-			Collection<FlowNode> list_nodes = getFlowingFlowNodes(start);
 			
-			for(FlowNode node : list_nodes) {
+			List<FlowNode> list_nodes = getFlowingFlowNodes(start); //Get all nodes
+			
+			//Save next nodes in the list
+			if(list_nodes.size() == 1) {
 				
-				if(list_nodes.contains(end)) {
-					
-					System.out.println("End node: " + node.getId() + "." + "\n\nEnd of path.");
-					//path.add(node);
+				System.out.println("Only 1 next node found: " + list_nodes.get(0).getId());
 
-				}else{ 
+				save(list_nodes.get(0).getId());
+
+				checkIfEnd(end, list_nodes);
+				
+				
+			}else{
+				
+				String s1 = "\nSeveral nodes were found: ";
+
+				for(int i = 0; i<list_nodes.size(); i++) {
 					
-					System.out.println("Next node: " + node.getId());
-					//Add possible paths here for the nodes
-					//find(node, end);
+					s1 += list_nodes.get(i).getId();
 					
+					if(i == list_nodes.size() -1) {
+						s1 += ".";
+					}else {
+						s1+= ", ";
+					}
+					
+
+					
+			
 				}
+				
+				System.out.println(s1);
+				
+		
 			}
 			
+			
+			
+
+
 		}catch(NullPointerException npe) {
 			//System.out.println("\nThe id the of the element does not exist");
 			System.exit(-1);
 		} 
 	}
 	
-	public Collection<FlowNode> getFlowingFlowNodes(FlowNode node) {
+	public void save(String node_to_save) {
 		
-		Collection<FlowNode> followingFlowNodes = new ArrayList<FlowNode>();
+
+		saved_nodes.add(node_to_save);
+		
+	}
+
+	
+
+	public List<FlowNode> getFlowingFlowNodes(FlowNode node) {
+		
+		List<FlowNode> followingFlowNodes = new ArrayList<FlowNode>();
 		
 		for (SequenceFlow sequenceFlow : node.getOutgoing()) {
 		  followingFlowNodes.add(sequenceFlow.getTarget());
